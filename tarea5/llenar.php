8495055ecf4b87297f66811c8f250c3601b02866
Rwin97 <54337147+Rwin97@users.noreply.github.com>
2020-03-25 15:57:44 -0400

    Add files via upload

---
+++ tarea5/llenar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<form action="restas.php" method="POST">
+
+<?php  
+$n=$_POST['n'];
+for ($i=0; $i<$n ; $i++) { 
+?>
+<input type="text" name="x<?php echo $i;?>">
+<?php 
+}
+ ?>
+<br>
+<?php for ($i=0; $i<$n ; $i++) {?>
+<input type="text" name="y<?php echo $i;?>">
+<?php } ?>
+
+<input type="hidden" name="n" value="<?php echo $n?>"><br><br>
+<input type="submit" name="restar"> 
+</form>
+</body>
+</html>
