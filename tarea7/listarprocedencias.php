289e9ec557bdbf8ac6d5f547f6631e2be935ba9c
Rwin97 <leonerwin890@gmail.com>
2020-07-06 17:34:05 -0400

    7 Corregido

--- tarea7/listarprocedencias.php
+++ tarea7/listarprocedencias.php
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 <?php 
 include('conexion.php');
-$sql="select Nombres, Apellidos, Sexo, Edad, Celular,Estado, Fecha, Procedencia, CodigoPais from perosnas, procedencia where perosnas.ID=procedencia.ID";
+$sql="select nombre, apellidos, sexo, edad, celular,estado, fecha, procedencia, codigoPais
+from persona, procedencias where persona.id=procedencias.id";
 $resultado=$con->query($sql);
  ?>
  <style>
@@ -30,15 +30,15 @@ th{
     {
       ?>
     <tr>
-    <td><?php echo $fila['Nombres'];?> </td>
-    <td><?php echo $fila['Apellidos'];?></td>
-    <td><?php echo $fila['Edad'];?></td>
-    <td><?php echo $fila['Sexo'];?></td>
-    <td><?php echo $fila['Celular'];?></td>
-    <td><?php echo $fila['Estado'];?></td>
-    <td><?php echo $fila['Fecha'];?></td>
-    <td><?php echo $fila['Procedencia'];?> </td>
-    <td><?php echo $fila['CodigoPais'];?></td>
+    <td><?php echo $fila['nombre'];?> </td>
+    <td><?php echo $fila['apellidos'];?></td>
+    <td><?php echo $fila['edad'];?></td>
+    <td><?php echo $fila['sexo'];?></td>
+    <td><?php echo $fila['celular'];?></td>
+    <td><?php echo $fila['estado'];?></td>
+    <td><?php echo $fila['fecha'];?></td>
+    <td><?php echo $fila['procedencia'];?> </td>
+    <td><?php echo $fila['codigoPais'];?></td>
     </tr>
     <?php 
     }
@@ -46,53 +46,4 @@ th{
   </tr>
 
 </table>
-=======
-<?php 
-include('conexion.php');
-$sql="select Nombres, Apellidos, Sexo, Edad, Celular,Estado, Fecha, Procedencia, CodigoPais from perosnas, procedencia where perosnas.ID=procedencia.ID";
-$resultado=$con->query($sql);
- ?>
- <style>
-table, th, td{
-border: 1px solid blue;
-}   
-th{
-  background: blue;
-} 
- </style>
- <center>
- <table>
-  <th>Nombres</th>
- 	<th>Apellidos</th>
- 	<th>Sexo</th>
- 	<th>Edad</th>
-  <th>Celular</th>
-  <th>Estado</th>
-  <th>Fecha</th> 
- 	<th>Procedencia</th>
- 	<th>CodigoPais</th>
-    <tr>
-
-   <?php
-    while ($fila=$resultado->fetch_assoc())
-    {
-      ?>
-    <tr>
-    <td><?php echo $fila['Nombres'];?> </td>
-    <td><?php echo $fila['Apellidos'];?></td>
-    <td><?php echo $fila['Edad'];?></td>
-    <td><?php echo $fila['Sexo'];?></td>
-    <td><?php echo $fila['Celular'];?></td>
-    <td><?php echo $fila['Estado'];?></td>
-    <td><?php echo $fila['Fecha'];?></td>
-    <td><?php echo $fila['Procedencia'];?> </td>
-    <td><?php echo $fila['CodigoPais'];?></td>
-    </tr>
-    <?php 
-    }
-    ?>
-  </tr>
-
-</table>
->>>>>>> e7491393ab60aad01aed9c79e3f5765045050072
 </center>
